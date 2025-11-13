323d98f56950c68b469c79828d974322460730c1
Oliver Koerber <oliver.koerber@it-forum.de>
2025-11-13 11:49:25 +0100

    Step 36: Device Adaptation

--- webapp/component.js
+++ webapp/component.js
@@ -1,45 +1,46 @@
 sap.ui.define([
-	"sap/ui/core/UIComponent",
-	"sap/ui/model/json/JSONModel",
-	"sap/ui/model/resource/ResourceModel",
-	"./controller/HelloDialog",
-	"sap/ui/Device"
+  "sap/ui/core/UIComponent",
+  "sap/ui/model/json/JSONModel",
+  "sap/ui/model/resource/ResourceModel",
+  "./controller/HelloDialog",
+  "sap/ui/Device"
 ], function(UIComponent, JSONModel, ResourceModel, HelloDialog, Device) {
-	"use strict";
-	return UIComponent.extend("sap.ui.demo.walkthrough.component", {
-		metadata: {
-			manifest: "json"
-		},
-		init: function() {
-			console.log("Initializing component...");
-			// call the init function of the parent
-			UIComponent.prototype.init.apply(this, arguments);
-			// set device model
-			const oDeviceModel = new JSONModel(Device);
-			oDeviceModel.setDefaultBindingMode("OneWay");
-			this.setModel(oDeviceModel, "device")
-				;
-			const oInvoiceModel = new sap.ui.model.odata.v2.ODataModel(
-				"https://services.odata.org/V2/Northwind/Northwind.svc/"
-			);
-			this.setModel(oInvoiceModel, "invoice");
-			// set data models
-			var oData = {
-				recipient: {
-					name: "UI5"
-				}
-			};
-			var oModel = new JSONModel(oData);
-			this.setModel(oModel);
-			// set dialog
-			let oRootControl = this.getRootControl();
-			console.log(`Creating HelloDialog with view ${oRootControl}`);
-			this._helloDialog = new HelloDialog(oRootControl);
-			this.getRouter().initialize();
-		},
-		openHelloDialog: function() {
-			console.log("component opens dialog");
-			this._helloDialog.open();
-		}
-	});
+  "use strict";
+  return UIComponent.extend("sap.ui.demo.walkthrough.component", {
+    metadata: {
+      manifest: "json"
+    },
+    init: function() {
+      console.log("Initializing component...");
+      // call the init function of the parent
+      UIComponent.prototype.init.apply(this, arguments);
+
+      // set device model
+      const oDeviceModel = new JSONModel(Device);
+      oDeviceModel.setDefaultBindingMode("OneWay");
+      this.setModel(oDeviceModel, "device");
+
+      const oInvoiceModel = new sap.ui.model.odata.v2.ODataModel(
+        "https://services.odata.org/V2/Northwind/Northwind.svc/"
+      );
+      this.setModel(oInvoiceModel, "invoice");
+      // set data models
+      var oData = {
+        recipient: {
+          name: "UI5"
+        }
+      };
+      var oModel = new JSONModel(oData);
+      this.setModel(oModel);
+      // set dialog
+      let oRootControl = this.getRootControl();
+      console.log(`Creating HelloDialog with view ${oRootControl}`);
+      this._helloDialog = new HelloDialog(oRootControl);
+      this.getRouter().initialize();
+    },
+    openHelloDialog: function() {
+      console.log("component opens dialog");
+      this._helloDialog.open();
+    }
+  });
 });
--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -2,13 +2,19 @@ sap.ui.define([
   "sap/ui/core/mvc/Controller",
   "sap/ui/core/UIComponent",
   "sap/ui/core/routing/History",
-  "sap/m/MessageToast"
-], function(Controller, UIComponent, History, MessageToast) {
+  "sap/m/MessageToast",
+  "sap/ui/model/json/JSONModel"
+], function(Controller, UIComponent, History, MessageToast, JSONModel) {
   "use strict"
   return Controller.extend("sap.ui.demo.walkthrough.controller.Detail", {
     onInit: function() {
       var oRouter = UIComponent.getRouterFor(this);
       oRouter.getRoute("detail").attachPatternMatched(this._onObjectMatched, this);
+
+      var oViewModel = new JSONModel({
+        "currency": "EUR"
+      });
+      this.getView().setModel(oViewModel, "view");
     },
     _onObjectMatched: function(oEvent) {
       console.log("View 'Detail': \n\tShipperName: {invoice>ShipperName}", "\n\tProductName: {invoice>ProductName}");
